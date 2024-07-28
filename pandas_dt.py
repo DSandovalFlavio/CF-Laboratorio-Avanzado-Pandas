8444597c4f98b5912115832684cc256a4ca796d8
dsandovalflavio <flaviocesarsandoval@gmail.com>
2024-07-28 16:16:52 -0600

    update

--- pandas_dt.py
+++ pandas_dt.py
@@ -1,2 +1,11 @@
 # %%
 import pandas as pd
+df = pd.read_csv('https://raw.githubusercontent.com/DSandovalFlavio/CF-Laboratorio-Avanzado-Pandas/main/data/walmartsales.csv')
+# %%
+df.info()
+# %%
+df['fecha'] = pd.to_datetime(df['fecha'], format='%Y-%m-%d') # Formato estándar
+df['fecha'] = pd.to_datetime(df['Date'], format='%d-%m-%Y')  
+# %%
+df['año'] = df['fecha'].dt.year
+df['mes_nombre'] = df['fecha'].dt.month_name()
